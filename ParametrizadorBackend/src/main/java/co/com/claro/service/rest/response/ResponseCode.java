cde677b8ac929c317889766da679d75a62a26d35
Andres Bedoya <andresbedoya@Andress-iMac.local>
2018-07-30 16:01:14 -0500

    Ajuste codigo error 409

--- ParametrizadorBackend/src/main/java/co/com/claro/service/rest/response/ResponseCode.java
+++ ParametrizadorBackend/src/main/java/co/com/claro/service/rest/response/ResponseCode.java
@@ -10,31 +10,19 @@ package co.com.claro.service.rest.response;
  * @author andresbedoya
  */
 public enum ResponseCode implements HttpCodeType {
-    
+
     OK(200, "Success"),
-
     BAD_REQUEST(400, "Bad Request"),
-
     UNAUTHORIZED(401, "Not Authorized"),
-
     FORBIDDEN(403, "Forbidden"),
-
     NOT_FOUND(404, "Not found"),
-
     METHOD_NOT_ALLOWED(405, "Method not allowed"),
-    
     CONFLICT(409, "Conflicto en la base de datos"),
-
     UNSUPPORTED_MEDIA_TYPE(415, "Unsopported media type"),
-
-    NOT_ALLOW_NULL(418,"Not Allow null"),
-
-    NOT_ALLOWED_REPEAT(419,"Not Allowed Repeat"),
-
-    NO_RESOURCES(420,"No Resources"),
-
+    NOT_ALLOW_NULL(418, "Not Allow null"),
+    NOT_ALLOWED_REPEAT(419, "Not Allowed Repeat"),
+    NO_RESOURCES(420, "No Resources"),
     ERROR_JSON(499, "Error json"),
-
     INTERNAL_SERVER_ERROR(500, "Internal Server Error");
 
     private int code;
@@ -42,26 +30,26 @@ public enum ResponseCode implements HttpCodeType {
     private String msg;
 
     ResponseCode(int code, String msg) {
-            this.code = code;
-            this.msg = msg;
+        this.code = code;
+        this.msg = msg;
     }
 
     @Override
     public int getCode() {
-            return code;
+        return code;
     }
 
     public void setCode(int code) {
-            this.code = code;
+        this.code = code;
     }
 
     @Override
     public String getMsg() {
-            return msg;
+        return msg;
     }
 
     public void setMsg(String msg) {
-            this.msg = msg;
+        this.msg = msg;
     }
 
 }
