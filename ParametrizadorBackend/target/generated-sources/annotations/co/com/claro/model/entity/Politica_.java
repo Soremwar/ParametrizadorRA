45bb6c22aa8eb439b30e202e455c7659b0219ed1
Andres Bedoya <andresbedoya@Andress-iMac.local>
2018-10-03 06:41:46 -0500

    Ajustes errores despues de pruebas

--- ParametrizadorBackend/target/generated-sources/annotations/co/com/claro/model/entity/Politica_.java
+++ ParametrizadorBackend/target/generated-sources/annotations/co/com/claro/model/entity/Politica_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-29T09:45:18")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-02T10:07:15")
 @StaticMetamodel(Politica.class)
 public class Politica_ { 
 
