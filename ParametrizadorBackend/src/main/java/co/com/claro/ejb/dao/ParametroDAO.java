09c2c726d35df987d4b4d08c1e9fd0a54989bce5
RonnieDev0 <ronald.ramirez@outlook.com>
2018-11-25 23:49:19 -0500

    Se agrega servicio que permite consultar parámetros a vencer de CONCILIACION

--- ParametrizadorBackend/src/main/java/co/com/claro/ejb/dao/ParametroDAO.java
+++ ParametrizadorBackend/src/main/java/co/com/claro/ejb/dao/ParametroDAO.java
@@ -8,6 +8,9 @@ package co.com.claro.ejb.dao;
 import co.com.claro.ejb.dao.parent.AbstractJpaDAO;
 import co.com.claro.model.entity.Parametro;
 import co.com.claro.service.rest.excepciones.DataNotFoundException;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,13 +21,14 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
 /**
- * Definimos una subinterface por cada entidad que queremos persistir. para asi poder
- * agregar metodos propios de esta entidad y heredar los generales.
+ * Definimos una subinterface por cada entidad que queremos persistir. para asi
+ * poder agregar metodos propios de esta entidad y heredar los generales.
+ *
  * @author Andres Bedoya
  */
-
 @Stateless
-public class ParametroDAO extends AbstractJpaDAO<Parametro>{
+public class ParametroDAO extends AbstractJpaDAO<Parametro> {
+
     private static final Logger logger = Logger.getLogger(ParametroDAO.class.getSimpleName());
     @PersistenceContext(unitName = "co.com.claro_ParametrizadorClaro_war_1.0PU")
     private EntityManager em;
@@ -37,30 +41,59 @@ public class ParametroDAO extends AbstractJpaDAO<Parametro>{
     protected EntityManager getEntityManager() {
         return em;
     }
-    
+
     /**
      * Retornar todos los registros
+     *
      * @param range
      * @return Retorna todos los registros
      */
-    public List<Parametro> findByAllTree(int[] range){
+    public List<Parametro> findByAllTree(int[] range) {
         TypedQuery<Parametro> query = em.createNamedQuery("Parametro.findAll", Parametro.class);
         query.setMaxResults(range[1]);// - range[0] + 1);
-        query.setFirstResult(range[0]);           
+        query.setFirstResult(range[0]);
         List<Parametro> lst = query.getResultList();
         if (lst == null || lst.isEmpty()) {
             throw new DataNotFoundException("No se encontraron datos");
-        }        
+        }
         return query.getResultList();
-    }  
+    }
+
+    public List<Parametro> findAVencer() {
+        List<Parametro> parametroPeriodicidad = this.findByAnyColumn("periodicidadparamconciliacion");
+        int periodicidad = 30; // 30 días es el valor por defecto
+        if (parametroPeriodicidad.size() > 0) {
+            try {
+                periodicidad = Integer.parseInt(parametroPeriodicidad.get(0).getValor());
+            } catch (Exception ex) {
+
+            }
+        }
+        Date fechaActual = new Date();
+          Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
+            calendar.setTime(fechaActual);   // assigns calendar to given date 
+            calendar.add(Calendar.DAY_OF_MONTH, (periodicidad*-1));
+
+        TypedQuery<Parametro> query = em.createNamedQuery("Parametro.findAVencer", Parametro.class);
+        query.setParameter("fecha",fechaActual);
+        List<Parametro> results = query.getResultList();
+        System.out.println("A vencer:**"+results.size());
+        if (results == null || results.isEmpty()) {
+            throw new DataNotFoundException("No se encontraron datos de Busqueda");
+        }
+        return results;
+
+    }
 
     /**
      * Buscar la misma cadena en todos los campos descriptivos
-     * @param busqueda Campo por el cual va a buscar en todos los campos descriptivos
+     *
+     * @param busqueda Campo por el cual va a buscar en todos los campos
+     * descriptivos
      * @return Lista de Parametros que cumplan con el criterio
      */
-    public List<Parametro> findByAnyColumn(String busqueda){
-        logger.log(Level.INFO, "busqueda:{0}", new Object[]{busqueda});     
+    public List<Parametro> findByAnyColumn(String busqueda) {
+        logger.log(Level.INFO, "busqueda:{0}", new Object[]{busqueda});
         TypedQuery<Parametro> query = em.createNamedQuery("Parametro.findByAnyColumn", Parametro.class);
         query.setParameter("parametro", "%" + busqueda + "%");
         query.setParameter("descripcion", "%" + busqueda + "%");
@@ -72,14 +105,16 @@ public class ParametroDAO extends AbstractJpaDAO<Parametro>{
         }
         return results;
     }
-    
-        /**
+
+    /**
      * Buscar un registro por un tupla de tipo y codpadre
-     * @param busqueda Campo por el cual va a buscar en todos los campos descriptivos
+     *
+     * @param busqueda Campo por el cual va a buscar en todos los campos
+     * descriptivos
      * @return Lista de Parametros que cumplan con el criterio
      */
-    public List<Parametro> findByCodPadre(String tipo, Integer codPadre){
-        logger.log(Level.INFO, "tipo:{0}codPadre:{1}", new Object[]{tipo,codPadre});     
+    public List<Parametro> findByCodPadre(String tipo, Integer codPadre) {
+        logger.log(Level.INFO, "tipo:{0}codPadre:{1}", new Object[]{tipo, codPadre});
         TypedQuery<Parametro> query = em.createNamedQuery("Parametro.findByCodPadre", Parametro.class);
         query.setParameter("tipo", "%" + tipo + "%");
         //query.setParameter("codigoPadre", "%" + codPadre + "%");
@@ -90,15 +125,14 @@ public class ParametroDAO extends AbstractJpaDAO<Parametro>{
         }
         return results;
     }
-    
-    public String findByParametro(String tipo, String parametro){
-        logger.log(Level.INFO, "tipo:{0}nombre:{1}", new Object[]{tipo,parametro});     
+
+    public String findByParametro(String tipo, String parametro) {
+        logger.log(Level.INFO, "tipo:{0}nombre:{1}", new Object[]{tipo, parametro});
         TypedQuery<Parametro> query = em.createNamedQuery("Parametro.findByParametro", Parametro.class);
         query.setParameter("tipo", tipo);
         query.setParameter("parametro", parametro);
         List<Parametro> result = query.getResultList();
         return result.get(0).getValor();
     }
-   
-}
 
+}
--- ParametrizadorBackend/src/main/java/co/com/claro/service/rest/ParametrosREST.java
+++ ParametrizadorBackend/src/main/java/co/com/claro/service/rest/ParametrosREST.java
@@ -40,26 +40,28 @@ import javax.ws.rs.core.Response;
  */
 @Stateless
 @Path("parametros")
-public class ParametrosREST{
+public class ParametrosREST {
+
     @Transient
     private static final Logger logger = Logger.getLogger(ParametrosREST.class.getSimpleName());
-    
+
     private String usuario = "admin";
     private String modulo = "parametros";
-    
+
     @EJB
     protected LogAuditoriaDAO logAuditoriaDAO;
     @EJB
     protected ParametroDAO managerDAO;
-    
+
     //@EJB
     //protected EscenarioDAO padreDAO;
-    
-       /**
+    /**
      * Obtiene las Parametros Paginadas
+     *
      * @param offset Desde cual item se retorna
      * @param limit Limite de items a retornar
-     * @param orderby Indica por cual campo descriptivo va a guardar (id, nombre, fechaCreacion)
+     * @param orderby Indica por cual campo descriptivo va a guardar (id,
+     * nombre, fechaCreacion)
      * @return Toda la lista de escenarios que corresponden con el criterio
      */
     @GET
@@ -68,48 +70,59 @@ public class ParametrosREST{
             @QueryParam("offset") int offset,
             @QueryParam("limit") int limit,
             @QueryParam("orderby") String orderby) {
-        logger.log(Level.INFO, "offset:{0}limit:{1}orderby:{2} ", new Object[]{offset, limit, orderby});     
+        logger.log(Level.INFO, "offset:{0}limit:{1}orderby:{2} ", new Object[]{offset, limit, orderby});
         List<Parametro> lst = managerDAO.findRange(new int[]{offset, limit});
         List<ParametroDTO> lstDTO = lst.stream().map(item -> item.toDTO()).distinct().sorted(comparing(ParametroDTO::getId).reversed()).collect(toList());
         //lstDTO = UtilListas.ordenarListaParametros(lstDTO, orderby);
-        List<ParametroDTO> lstFinal = (List<ParametroDTO>)(List<?>) lstDTO;
+        List<ParametroDTO> lstFinal = (List<ParametroDTO>) (List<?>) lstDTO;
         return lstFinal;
     }
-    
+
+    @GET
+    @Path("/findAVencer")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<ParametroDTO> findAVencer() {
+        List<Parametro> lst = managerDAO.findAVencer();
+        return lst.stream().map(item->item.toDTO()).collect(toList());
+    }
+
     /**
      * Obtiene un indicador por id
+     *
      * @param id Identificador de conciliacion
      * @return Una Escenario que coincide con el criterio
      */
     @GET
     @Path("{id}")
     @Produces({MediaType.APPLICATION_JSON})
-    public ParametroDTO getById(@PathParam("id") int id){
+    public ParametroDTO getById(@PathParam("id") int id) {
         logger.log(Level.INFO, "id:{0}", id);
         Parametro entidad = managerDAO.find(id);
         return entidad.toDTO();
 
     }
-    
+
     /**
      * Busca los indicadores por cualquier columna
+     *
      * @param texto Texto a buscar en cualquier texto
      * @return Lista de Escenarios que cumplen con el criterio
      */
     @GET
     @Path("/findByAny")
     @Produces({MediaType.APPLICATION_JSON})
-    public List<ParametroDTO> findByAnyColumn(@QueryParam("texto") String texto){
-        logger.log(Level.INFO, "texto:{0}", texto);      
+    public List<ParametroDTO> findByAnyColumn(@QueryParam("texto") String texto) {
+        logger.log(Level.INFO, "texto:{0}", texto);
         List<Parametro> lst = managerDAO.findByAnyColumn(texto);
         List<ParametroDTO> lstDTO = lst.stream().map(item -> item.toDTO()).sorted(comparing(ParametroDTO::getId)).collect(toList());
-        List<ParametroDTO> lstFinal = (List<ParametroDTO>)(List<?>) lstDTO;
-        return lstFinal;        
+        List<ParametroDTO> lstFinal = (List<ParametroDTO>) (List<?>) lstDTO;
+        return lstFinal;
     }
-    
-    
-        /**
-     * Busca los Parametros por algun padre. Recuerden que la relacion es logica no fisica
+
+    /**
+     * Busca los Parametros por algun padre. Recuerden que la relacion es logica
+     * no fisica
+     *
      * @param tipo
      * @param codPadre
      * @return Lista de Escenarios que cumplen con el criterio
@@ -117,33 +130,35 @@ public class ParametrosREST{
     @GET
     @Path("/padre")
     @Produces({MediaType.APPLICATION_JSON})
-    public List<ParametroDTO>  findByPadre(@QueryParam("tipo") String tipo, @QueryParam("codpadre") Integer codPadre){
-        logger.log(Level.INFO, "tipo:{0}codPadre:{1}", new Object[]{tipo, codPadre});      
+    public List<ParametroDTO> findByPadre(@QueryParam("tipo") String tipo, @QueryParam("codpadre") Integer codPadre) {
+        logger.log(Level.INFO, "tipo:{0}codPadre:{1}", new Object[]{tipo, codPadre});
         List<Parametro> lst = managerDAO.findByCodPadre(tipo, codPadre);
         List<ParametroDTO> lstDTO = lst.stream().map(item -> item.toDTO()).sorted(comparing(ParametroDTO::getId)).collect(toList());
-        List<ParametroDTO> lstFinal = (List<ParametroDTO>)(List<?>) lstDTO;
-        return lstFinal;     
+        List<ParametroDTO> lstFinal = (List<ParametroDTO>) (List<?>) lstDTO;
+        return lstFinal;
     }
 
     /**
      * Crea una nueva entidad
+     *
      * @param entidad Entidad que se va a agregar
      * @return el la entidad recien creada
      */
     @POST
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
-    public Response add(ParametroDTO entidad)  {
+    public Response add(ParametroDTO entidad) {
         logger.log(Level.INFO, "entidad:{0}", entidad);
         Parametro entidadJPA = entidad.toEntity();
         managerDAO.create(entidadJPA);
         LogAuditoria logAud = new LogAuditoria(this.modulo, Constantes.Acciones.AGREGAR.name(), Date.from(Instant.now()), usuario, entidadJPA.toString());
         logAuditoriaDAO.create(logAud);
         return Response.status(Response.Status.CREATED).entity(entidadJPA.toDTO()).build();
-    }  
-    
+    }
+
     /**
      * Actualiza la entidad por su Id
+     *
      * @param entidad conciliacion con la cual se va a trabajar
      * @return el resultado de la operacion
      */
@@ -151,7 +166,7 @@ public class ParametrosREST{
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     public Response update(ParametroDTO entidad) {
-        logger.log(Level.INFO, "entidad:{0}", entidad);  
+        logger.log(Level.INFO, "entidad:{0}", entidad);
         Parametro entidadJPA = managerDAO.find(entidad.getId());
         if (entidadJPA != null) {
             entidadJPA.setFechaActualizacion(Date.from(Instant.now()));
@@ -166,11 +181,12 @@ public class ParametrosREST{
             return Response.status(Response.Status.OK).entity(entidadJPA.toDTO()).build();
         }
         return Response.status(Response.Status.NOT_FOUND).build();
-      
+
     }
-    
+
     /**
      * Borra una entidad por su Id
+     *
      * @param id Identificador de la identidad
      * @return el resultado de la operacion
      */
@@ -186,11 +202,11 @@ public class ParametrosREST{
         WrapperResponseEntity mensaje = new WrapperResponseEntity(Response.Status.OK.getStatusCode(), Response.Status.OK.getReasonPhrase(), "Registro borrado exitosamente");
         return Response.status(Response.Status.OK).entity(mensaje).build();
     }
-    
+
     @GET
     @Path("/count")
     @Produces({MediaType.APPLICATION_JSON})
-    public int count(){
+    public int count() {
         return managerDAO.count();
     }
 }
